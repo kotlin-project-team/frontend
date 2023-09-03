28e1e5e48f1e45bff8b6f1735e73973b5b4eaa6d
JaeHyeonSim <peanutandnuts@gmail.com>
2023-09-04 02:30:47 +0900

    feat: post room db 적용 [#8]

--- dongamboard/app/src/main/java/com/study/dongamboard/model/PostData.kt
+++ dongamboard/app/src/main/java/com/study/dongamboard/model/PostData.kt
@@ -1,15 +1,22 @@
 package com.study.dongamboard.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.study.dongamboard.Converters
 import java.io.Serializable
 import java.sql.Timestamp
 
-data class PostData(var id: Int,
-                    var userId: Int,
-                    var title: String,
-                    var content: String,
-                    var category: String,
-                    var likes: Int,
-                    var views: Int,
-                    var createdAt: Timestamp,
-                    var updatedAt: Timestamp
+@Entity(tableName = "post_table")
+data class PostData(
+    @PrimaryKey
+    var id: Int,
+    var userId: Int,
+    var title: String,
+    var content: String,
+    var category: String,
+    var likes: Int,
+    var views: Int
+//    var createdAt: Timestamp,
+//    var updatedAt: Timestamp
 ) : Serializable
