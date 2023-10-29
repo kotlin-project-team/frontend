9266b9f1f58c09160cb6a07bec41fcf1192ad4c5
JaeHyeonSim <peanutandnuts@gmail.com>
2023-10-29 19:41:43 +0900

    :sparkles: feat: Post api 연결 [#18]

--- dongamboard/app/src/main/java/com/study/dongamboard/api/APIService.kt
+++ dongamboard/app/src/main/java/com/study/dongamboard/api/APIService.kt
@@ -1,11 +1,22 @@
 package com.study.dongamboard.api
 
 import com.study.dongamboard.model.PostData
+import com.study.dongamboard.model.request.PostRequest
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface APIService {
+    @GET("/api/post")
+    suspend fun getAllPost(): List<PostData>
 
-    @GET("/posts")  // test를 위한 url, 추후 board로 변경
-    suspend fun getAllPosts(): List<PostData>
+    @GET("/api/post/{postId}")
+    suspend fun getPostById(@Path(value = "postId") id: Int): PostData
 
+    @POST("/api/post")
+    suspend fun createPost(@Body postRequest: PostRequest)
+
+    @POST("/api/post/like/{postId}")
+    suspend fun clickPostLike(@Path(value = "postId") id: Int)
 }
