4164d9c19ee686303ac6baf6ffee3e4f26ad4d53
JaeHyeonSim <peanutandnuts@gmail.com>
2023-12-17 20:17:32 +0900

    :sparkles: feat: notice u&d UI 동작 구현 [#37]

---
+++ dongamboard/app/src/main/java/com/study/dongamboard/activity/notice/NoticeUpdateActivity.kt
@@ -0,0 +1,49 @@
+package com.study.dongamboard.activity.notice
+
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.study.dongamboard.R
+import com.study.dongamboard.api.APIObject
+import com.study.dongamboard.db.NoticeDB
+import com.study.dongamboard.model.request.NoticeRequest
+import com.study.dongamboard.model.response.NoticeResponse
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoticeUpdateActivity : AppCompatActivity() {
+
+    lateinit var notice: NoticeResponse
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write)
+
+        notice = intent.getSerializableExtra("noticeData") as NoticeResponse
+
+        // TODO: title 추가 시 설정
+//        val etPostCreateTitle = findViewById<EditText>(R.id.etPostCreateTitle)
+        val etPostCreateContent = findViewById<EditText>(R.id.etPostCreateContent)
+
+//        etPostCreateTitle.setText(notice.title)
+        etPostCreateContent.setText(notice.content)
+
+        val ivCreatePostBtn = findViewById<ImageView>(R.id.ivCreatePostBtn)
+        ivCreatePostBtn.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val noticeRequest = NoticeRequest(
+                    etPostCreateContent.text.toString()
+                )
+                APIObject.getRetrofitAPIService.updateNotice(notice.id, noticeRequest)
+                finish()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        NoticeDB.destroyInstance()
+        super.onDestroy()
+    }
+}
