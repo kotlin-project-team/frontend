0236cbe8423c5e1706f2724f114cadc4f45969af
Sim JaeHyeon <112995965+JaeHyeonSim@users.noreply.github.com>
2024-01-28 21:40:13 +0900

    :recycle: refactor: public -> private

--- app/src/main/java/com/study/dongamboard/api/APIObject.kt
+++ app/src/main/java/com/study/dongamboard/api/APIObject.kt
@@ -8,7 +8,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object APIObject {
-    const val BASE_URL ="http://10.0.2.2:8080/"
+    private const val BASE_URL ="http://10.0.2.2:8080/"
 
     val moshi = Moshi.Builder()
         .addLast(KotlinJsonAdapterFactory())
